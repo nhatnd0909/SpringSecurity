3cf64debea4b6b4a424e90b71362e5c62bc8b65b
nhatnd0909 <nguyendinhnhat0909@gmail.com>
2024-09-28 21:37:33 +0700

    Add repository

--- src/main/java/com/example/demoSpringSecurity/model/UserEntity.java
+++ src/main/java/com/example/demoSpringSecurity/model/UserEntity.java
@@ -2,10 +2,19 @@ package com.example.demoSpringSecurity.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
 	private long id;
 	private String email;
 
--- src/main/java/com/example/demoSpringSecurity/service/UserService.java
+++ src/main/java/com/example/demoSpringSecurity/service/UserService.java
@@ -2,39 +2,25 @@ package com.example.demoSpringSecurity.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demoSpringSecurity.model.UserEntity;
+import com.example.demoSpringSecurity.repository.UserRepository;
 
 @Service
 public class UserService {
-//	autowired repo
+	@Autowired
+	private UserRepository userRepository;
 
-	 private static final String EXISTING_EMAIL = "admin@test.com";
-	    private static final String OTHER_EMAIL = "user@test.com";
+	public Optional<UserEntity> findByEmail(String email) {
 
-	    public Optional<UserEntity> findByEmail(String email) {
-	        // All of this code would come from the database, but for simplicity of this example
-	        // Let's just fake it
+		var user = userRepository.findByEmail(email);
+		System.out.println(user);
+		if (user.isPresent()) {
+			return user;
+		}
+		return Optional.empty();
 
-	        if (EXISTING_EMAIL.equalsIgnoreCase(email)) {
-	            var user = new UserEntity();
-	            user.setId(1L);
-	            user.setEmail(EXISTING_EMAIL);
-	            user.setPassword("$2a$12$OBnerD3ZrnkqY/ofkaxune1jnpUscFhTGCcuVA9x5lgAGAtr6Bss2"); // test
-	            user.setRole("ROLE_ADMIN");
-	            user.setName("My nice admin user");
-	            return Optional.of(user);
-	        } else if (OTHER_EMAIL.equalsIgnoreCase(email)) {
-	            var user = new UserEntity();
-	            user.setId(99L);
-	            user.setEmail(OTHER_EMAIL);
-	            user.setPassword("$2a$12$OBnerD3ZrnkqY/ofkaxune1jnpUscFhTGCcuVA9x5lgAGAtr6Bss2"); // test
-	            user.setRole("ROLE_USER");
-	            user.setName("My nice simple user");
-	            return Optional.of(user);
-	        } else {
-	            return Optional.empty();
-	        }
-	    }
+	}
 }
